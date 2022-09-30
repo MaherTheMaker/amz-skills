bf5ff377bfb0d50cf0b9425bd93a6641b46e9a04
MaherMaker <maher.serawan.1998@gmail.com>
2022-09-30 18:52:25 +0300

    aaa

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import LandingPage from './Skills/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <LandingPage></LandingPage>
+      </header>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Skills/LandingPage.tsx
@@ -0,0 +1,14 @@
+import '../../node_modules/bootstrap/dist/css/bootstrap.css'
+export default function LandingPage() {
+
+
+    return <>
+    <div >
+        <h1>
+            Welcome To AMZ Skills 
+        </h1>
+        <br></br>
+        <button type="button" className="btn btn-danger">Let's Find out </button>
+        </div>
+    </>
+}
